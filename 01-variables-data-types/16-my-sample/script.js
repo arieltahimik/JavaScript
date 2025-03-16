922ce68e2c0d784348a69f163671596e0b950f6f
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-03-16 16:03:34 -0500

    01-16 My Sample

---
+++ 01-variables-data-types/16-my-sample/script.js
@@ -0,0 +1,63 @@
+const con = document.getElementById("console");
+con.innerHTML="console.log(20, 'Hello', true);";
+
+const tab = document.getElementById("table");
+tab.innerHTML = `
+<pre>
+  console.table({
+  name: 'John',
+  email: 'john@not.gmail.com'
+  });
+</pre>`;
+
+const cmnt = document.getElementById("cmnt");
+cmnt.innerHTML=`
+<pre>
+// single line comment
+
+/*
+multi
+line
+comment
+*/
+</pre>
+`;
+
+const vars = document.getElementById("vars");
+vars.innerHTML=`
+<pre>
+Naming Conventions
+- Only letters, numbers, underscores and dollar signs
+- Can't start with a number
+
+Multi-Word Formatting
+- firstName  = camelCase
+- first_name = underscore
+- FirstName  = PascalCase
+- firstname  = lowercase
+
+Declaring Variables
+var morph   = 10;           // morph can be redeclared
+let leaveMe = 20;           // leaveMe cannot be redeclared
+const noChangeAllowed = 30; // noChangeAllowed cannot be assigned another value once assigned
+</pre>
+`;
+
+const dtypes = document.getElementById("dtypes");
+dtypes.innerHTML = `
+<pre>
+Data Types
+- String        -> const band = "The Beatles";
+- Number        -> const year = 2025; 
+                   const pi = 3.1416;
+- Boolean       -> const yes = true; 
+                   const no = false;
+- Null          -> const knull = null;
+- Undefined     -> const whereAmI;
+- Symbol        -> const id = Symbol(id);
+- BigInt        -> const n = 9007199254740991n;
+- Array         -> const numbers = [1, 2, 3, 4];
+- Object        -> const person = { name: 'Brad' };
+- Function      -> function sayHello() { console.log('Hello'); }
+</pre>
+`;
