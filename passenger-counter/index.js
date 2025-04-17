841a1a0dd1715a123484f7c27d60b50adc2b34de
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-04-16 21:25:24 -0500

    Passenger Counter

---
+++ passenger-counter/index.js
@@ -0,0 +1,9 @@
+//document.getElementById("count-el").innerText = 5;
+
+let count = 0
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
