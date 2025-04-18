76d74f2eec4b471bbb9a7dd6b0ecaa87f3bff1b8
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-04-17 23:29:37 -0500

    Passenger Counter

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,9 +1,17 @@
-//document.getElementById("count-el").innerText = 5;
-
-let count = 0
-let countEl = document.getElementById("count-el")
+let count = 0;
+let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+  count = count + 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";    // the last space is not displaying
+  //saveEl.innerText += countDash;  // not displayng 'hidden' contnent, e.g. space
+  saveEl.textContent += countStr
+  count = 0
+  countEl.textContent = 0
+  //console.log(countStr);
 }
