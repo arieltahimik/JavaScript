fdcf9b4686fa8379687196030f24b426b45d480e
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-04-22 01:05:03 -0500

    Basketball Scoreboard - New Game button, highlight lead score

--- basketball-scoreboard/index.js
+++ basketball-scoreboard/index.js
@@ -11,4 +11,22 @@ function addScore(team, points) {
         guestPoints += points;
         guestScore.textContent = guestPoints;
     }
+
+    if (homePoints > guestPoints) {
+        homeScore.style.color = "#FEF3C7";   // yellowish
+        guestScore.style.color = "#F94F6D";  // pinkish
+    } else if (guestPoints > homePoints) {
+        guestScore.style.color = "#FEF3C7"
+        homeScore.style.color = "#F94F6D";    
+    } else {
+        homeScore.style.color = "#F94F6D";    
+        guestScore.style.color = "#F94F6D";
+    }
+}
+
+function newGame() {
+    homePoints = 0;
+    guestPoints = 0;
+    homeScore.textContent = 0;
+    guestScore.textContent=0;
 }
