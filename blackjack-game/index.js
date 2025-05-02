bf55a568d4366ba35ac73831b34f11d3e597199d
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-05-01 22:33:41 -0500

    Blackjack Game - initial commit

---
+++ blackjack-game/index.js
@@ -0,0 +1,7 @@
+let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+let sum = firstCard + secondCard;
+
+console.log(`firstCard ${firstCard}`);
+console.log(`secondCard ${secondCard}`);
+console.log(`sum ${sum}`);
