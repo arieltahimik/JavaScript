61ec07b09715dee4e40a127ab97fb3ed26d6f6d8
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-05-02 22:58:07 -0500

    Blackjack Game - booleans added

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,7 +1,25 @@
-let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-let sum = firstCard + secondCard;
+// let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+// let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
+// let sum = firstCard + secondCard;
 
-console.log(`firstCard ${firstCard}`);
-console.log(`secondCard ${secondCard}`);
-console.log(`sum ${sum}`);
+let firstCard = 10;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
+if (sum <= 20) {
+  console.log('Do you want to draw a new card? 🙂');
+} else if (sum === 21) {
+  console.log("Woohoo! You've got Blackjack! 🥳");
+  hasBlackJack = true;
+} else {
+  console.log("You're out of the game! 😭");
+  isAlive = false;
+}
+
+// CASH OUT!
+console.log('hasBlackJack: ', hasBlackJack);
+
+// OUT OF GAME!
+console.log('isAlive: ', isAlive);
