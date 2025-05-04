c64c2c518baf280f50c1274a739a3ad54ff451ae
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-05-03 23:14:34 -0500

    Blackjack Game - new css file added

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -3,23 +3,20 @@
 // let sum = firstCard + secondCard;
 
 let firstCard = 10;
-let secondCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
+let message = "";
 
 if (sum <= 20) {
-  console.log('Do you want to draw a new card? 🙂');
+  message = "Do you want to draw a new card? 🙂";
 } else if (sum === 21) {
-  console.log("Woohoo! You've got Blackjack! 🥳");
+  message = "Woohoo! You've got Blackjack! 🥳";
   hasBlackJack = true;
 } else {
-  console.log("You're out of the game! 😭");
+  message = "You're out of the game! 😭";
   isAlive = false;
 }
 
-// CASH OUT!
-console.log('hasBlackJack: ', hasBlackJack);
-
-// OUT OF GAME!
-console.log('isAlive: ', isAlive);
+console.log(message);
