dd6dc473ac7d76ecd9c60e8cfa7f6831a5955627
ArielT <157650875+arieltahimik@users.noreply.github.com>
2025-05-07 22:28:18 -0500

    Blackjack Game - updated message, button, and sum

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -8,16 +8,20 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+//let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
 
 function startGame() {
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Woohoo! You've got Blackjack! 🥳";
+    message = "Woohoo! You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
-  }
-  console.log(message);
+  }  
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sum;  // `Sum: ${sum}`
 }
